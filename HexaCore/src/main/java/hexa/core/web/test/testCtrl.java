9718f388fd817d63c0b9100bf267fb8bd614c256
LGPC <LGPC@Subbi>
2020-05-18 16:10:08 +0900

    Hexa Base

---
+++ HexaCore/src/main/java/hexa/core/web/test/testCtrl.java
@@ -0,0 +1,14 @@
+package hexa.core.web.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testCtrl {
+
+	@RequestMapping("/test.do")
+	public String Test() {
+		
+		return "test/test";
+	}
+}
