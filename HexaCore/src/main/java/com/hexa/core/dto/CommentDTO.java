05c9b45a8707856f2c5949e8b3c0ba52df3c2e39
gudsl <gudsl@LAPTOP-5050TEG5>
2020-05-26 16:50:25 +0900

    게시판 DTO : BbsDTO, 댓글 DTO : CommentDTO 작성하여 올립니다.

--- HexaCore/src/main/java/com/hexa/core/dto/CommentDTO.java
+++ HexaCore/src/main/java/com/hexa/core/dto/CommentDTO.java
@@ -1,5 +1,83 @@
 package com.hexa.core.dto;
 
-public class CommentDTO {
+import java.io.Serializable;
+
+public class CommentDTO implements Serializable{
+
+	private static final long serialVersionUID = 6394680361704233110L;
+	private int seq;
+	private String id;
+	private String name;
+	private String content;
+	private String regdate;
+	private int parent_seq;
+
+	public CommentDTO() {
+		super();
+	}
+
+	public CommentDTO(int seq, String id, String name, String content, String regdate, int parent_seq) {
+		super();
+		this.seq = seq;
+		this.id = id;
+		this.name = name;
+		this.content = content;
+		this.regdate = regdate;
+		this.parent_seq = parent_seq;
+	}
+
+	@Override
+	public String toString() {
+		return "CommentDTO [seq=" + seq + ", id=" + id + ", name=" + name + ", content=" + content + ", regdate="
+				+ regdate + ", parent_seq=" + parent_seq + "]";
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getParent_seq() {
+		return parent_seq;
+	}
+
+	public void setParent_seq(int parent_seq) {
+		this.parent_seq = parent_seq;
+	}
 
 }
