aeadab6ab140fd64335d43f5f73b849db541077c
LGPC <LGPC@Subbi>
2020-05-26 13:18:32 +0900

    임시저장

--- HexaCore/src/main/java/com/hexa/core/model/mng/inf/DepartmentIService.java
+++ HexaCore/src/main/java/com/hexa/core/model/mng/inf/DepartmentIService.java
@@ -1,13 +1,19 @@
 package com.hexa.core.model.mng.inf;
 
+import java.util.Map;
+
+import com.hexa.core.dto.DepartmentDTO;
+
 public interface DepartmentIService {
 
 
-	public boolean insertDept();
+	public boolean insertDept(DepartmentDTO dto);
 	
-	public boolean deleteDept();
+	public boolean deleteDept(String deptId);
 	
-	public boolean updateDept();
+	public boolean updateDept(DepartmentDTO dto);
+	
+	public Map<String, Object> selectDeptList();
 	
 	
 }
